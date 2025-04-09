c8b9327cd537edc0ecefcffb7a041156d54fbc4c
m3yevn <m3.luvusomuch@gmail.com>
2025-04-09 10:20:47 +0800

    fix: using axios directly which trigger server to return 418

--- lambda/getSmuleData.js
+++ lambda/getSmuleData.js
@@ -2,17 +2,21 @@ const axios = require("axios");
 
 async function getSmuleData({ offset, limit }) {
   const { SMULE_API, SMULE_ID } = process.env;
+  const axiosClient = new axios.Axios({
+    baseURL: `${SMULE_API}/profile/performances`,
+  });
   try {
-    const response = await axios.get(
-      `${SMULE_API}/profile/performances?accountId=${SMULE_ID}&appUid=sing&offset=${offset}&limit=${limit}`
+    const response = await axiosClient.get(
+      `?accountId=${SMULE_ID}&appUid=sing&offset=${offset}&limit=${limit}`
     );
     const { data } = response;
     return data;
   } catch (ex) {
     console.error(ex);
+    return new Error(ex.message);
   }
 }
 
 module.exports = {
-  getSmuleData
+  getSmuleData,
 };
--- routes.js
+++ routes.js
@@ -11,9 +11,13 @@ router.get("/profile", async (req, res) => {
   const { query } = req;
   const { offset = 0, limit = 0 } = query;
   const data = await getSmuleData({ offset, limit });
-  res.json(data);
+  res.json(
+    data instanceof Error
+      ? { error: { name: data.name, stack: data.stack, message: data.message } }
+      : data
+  );
 });
 
 module.exports = {
-  router
+  router,
 };
